3ee49f267c63d2981682df1a2e74eeb5c6c1bac5
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-09-17 19:56:35 -0300

    add imagens e criando header

--- littleinvest/src/App.js
+++ littleinvest/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import Header from './components/header/header'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
+        <Header />
       </div>
     );
   }
---
+++ littleinvest/src/components/header/header.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+// import Button from '../button/button.js'
+import './header.css';
+// import Logo from "../../assets/svg/little-invest.svg"
+
+const Header = () => {
+  return (
+    <header className="header-full">
+      <div>
+        {/* <img src={Logo} alt="logo" /> */}
+      </div>
+
+      <div>
+        <nav className="nav-menu">
+          <ul>
+            <li>Investimentos</li>
+            <li>Serviços</li>
+            <li>Contato</li>
+          </ul>
+        </nav>
+        {/* <Button /> */}
+      </div>
+    </header >
+  )
+}
+
+export default Header;
