244a11b8c7b41a0a42f11bd365a422a899e59b5f
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-10-06 16:26:44 -0300

    tentando conectar  com o banco de daos do firebase

---
+++ littleinvest/src/configure-firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAdBGHSp7u4G32O_qLOTA3bK7arzlBRX9U",
+    authDomain: "littleinvest-2516b.firebaseapp.com",
+    databaseURL: "https://littleinvest-2516b.firebaseio.com",
+    projectId: "littleinvest-2516b",
+    storageBucket: "littleinvest-2516b.appspot.com",
+    messagingSenderId: "657675669074",
+    appId: "1:657675669074:web:4b9d1bc58a38e5bfdd198b"
+});
+
+export default firebaseApp;
